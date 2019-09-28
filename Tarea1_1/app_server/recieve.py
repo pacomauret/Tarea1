408253c44206e56958c54d5a8c28c3f3e365fcee
pacomauret <francisco.fariasm@sansano.usm.cl>
2019-09-27 23:06:36 -0300

    Tarea 1

--- Tarea1_1/app_server/recieve.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("", 5000))
-s.listen(15)
-conn, addr = s.accept()
-print("accepted")
-print(bytes.decode(conn.recv(1024)))
--- Tarea1_1/app_server/send.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-command = "bridge test!"
-x=sock.connect(('172.17.0.1', 5000))
-print("this is x=",x)
-sock.sendall(command.encode())
