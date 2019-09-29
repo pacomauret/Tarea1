7ef66cfdf10cea3b38d0077ba3279d5bd151f9cd
francisco <francisco.solar@sansano.usm.cl>
2019-09-29 20:17:44 -0300

    servidor con while

--- Tarea1_1/send/Send.py
+++ Tarea1_1/send/Send.py
@@ -1,11 +1,21 @@
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#sock.bind(('0.0.0.0',5000))
-command = "bridge test!"
-adress= ('recieve',5000)
-sock.connect(('server', 5000))
-#x=sock.connect(adress)
-#print("this is x=",x)
-sock.sendall(command.encode())
-sock.close()
+#sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#command = "bridge test!"
+#adress= ('recieve', 5000)
+#sock.connect(('server', 5000))
+#sock.sendall(command.encode())
+#sock.close() 
+  
+s = socket.socket()   
+s.connect(('server', 5000))  
+  
+while True:  
+      mensaje = "quit"
+      s.sendall(mensaje.encode()) 
+      if mensaje == "quit":  
+         break  
+  
+print("adios")  
+  
+s.close() 
