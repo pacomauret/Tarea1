c088a13ffb72f1ee4723e9ba6b403658640a9945
francisco <francisco.solar@sansano.usm.cl>
2019-09-30 15:09:52 -0300

    faltan locks

--- Tarea1_2/Datanode_3/datanode.py
+++ Tarea1_2/Datanode_3/datanode.py
@@ -1,36 +1,51 @@
+from threading import Thread 
+from socketserver import ThreadingMixIn 
 import socket
+import time
+import struct
 
+class ClientThread(Thread): 
+    def __init__(self,ip,port): 
+        Thread.__init__(self) 
+        self.ip = ip 
+        self.port = port 
+        print("[+] New server socket thread started for " + ip + ":" + str(port)) 
 
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.bind(('server', 5000))
-#s.listen(9)
-#conn, addr = s.accept()
-#print("accepted")
-#print(bytes.decode(conn.recv(1024)))
+    def run(self):
+        if(self.ip == '224.1.1.1' ): #si esque es la thread para los pingeos constantes
+            while True:
+                MULTICAST_TTL = 2
+                mensaje = "holawa"
 
-#print ("hello")
- 
-s = socket.socket()
-s.bind(('headnode', 5000))  
-s.listen(1)  
+                sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+                sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                sock.bind((self.ip, self.port))
 
-sc, addr = s.accept()  
+                mreq = struct.pack("4sl", socket.inet_aton(self.ip), socket.INADDR_ANY)
+                sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+                print(bytes.decode(sock.recv(1024)))
 
-while True:  
-  recibido = bytes.decode(sc.recv(1024))  
-  if recibido == "Estado":
-     #print("Recibido:", recibido)  
-     #break
-    mensaje = "OK"
-    sc.send(mensaje.encode()) 
-  if recibido == "Quit":
-  	print("jabreak")
-  	break
+                mensaje = "Datanode3"
+                sock.sendall(mensaje.encode())
+            
 
-  #print("Recibido:", recibido)  
-  
+#----------------------Multicast Thread---------------------------------
+MCAST_GRP = '224.1.1.1'
+MCAST_PORT = 5001
+#Thread multicast se inicia en un principio para comenzar a pingear a los datanodes
+Datanode1 = ClientThread(MCAST_GRP,MCAST_PORT) #Aqui la ip deberia ser 'servicio' y el puerto 5000
+Datanode1.start()
+#-----------------------------------------------------------------------
 
-print("adios") 
+"""
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-sc.close()  
-s.close()  
+# on this port, listen ONLY to MCAST_GRP
+sock.bind((MCAST_GRP, MCAST_PORT))
+mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+data = bytes.decode(sock.recv(2048)) 
+print("Server received data:" + data)
+MESSAGE = input("Multithreaded Python server : Enter Response from Server/Enter exit:")
+sock.send(MESSAGE.encode())  # echo """
--- Tarea1_2/Headnode/headnode.py
+++ Tarea1_2/Headnode/headnode.py
@@ -1,33 +1,72 @@
+from threading import Thread 
+from socketserver import ThreadingMixIn 
 import socket
+import time
+
+class ClientThread(Thread): 
+
+	def __init__(self,ip,port): 
+		Thread.__init__(self) 
+		self.ip = ip 
+		self.port = port
+		self.Datanodes = ["Datanode1","Datanode2","Datanode3"] 
+		print("[+] New server socket thread started for " + ip + ":" + str(port)) 
+
+	def run(self):
+		if(self.ip == '224.1.1.1' ): #si esque es la thread para los pingeos constantes
+			while True:
+				for i in range(1,6):
+					print(i)
+					time.sleep(1)
+				MULTICAST_TTL = 2
+				mensaje = "holiwi"
 
 
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.bind(('server', 5000))
-#s.listen(9)
-#conn, addr = s.accept()
-#print("accepted")
-#print(bytes.decode(conn.recv(1024)))
+				sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+				
+				sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
 
-#print ("hello")
- 
-s = socket.socket()   
-s.bind(('headnode', 5000))  
-s.listen(1)  
+				sock.sendto(mensaje.encode(), (self.ip, self.port))
+				try:
+					print(bytes.decode(sock.recv(1024)))
+				except sock.error:
+					print("errorcetee")
 
-sc, addr = s.accept()  
+				
+				
+				#sock.bind((self.ip, self.port))
 
-while True:
-	mensaje = "estado"
-	s.sendall(mensaje.encode())
-	recibido = bytes.decode(sc.recv(1024))  
-	if recibido == "ok":
-		quit="quit" 
-		s.send(quit.encode())
-		break        
-	print("Recibido:", recibido)  
-	s.send(recibido.encode())  
+				
 
-print("adios") 
 
-sc.close()  
-s.close()  
+    		
+
+#----------------------Multicast Thread---------------------------------
+MCAST_GRP = '224.1.1.1'
+MCAST_PORT = 5001
+#Thread multicast se inicia en un principio para comenzar a pingear a los datanodes
+Datanode1 = ClientThread(MCAST_GRP,MCAST_PORT) #Aqui la ip deberia ser 'servicio' y el puerto 5000
+Datanode1.start()
+#-----------------------------------------------------------------------
+
+#-------------------------Manejo cliente--------------------------------
+"""TCP_IP = '' 
+TCP_PORT = 5000 
+BUFFER_SIZE = 1024  # Usually 1024, but we need quick response
+
+Server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+Server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+Server.bind((TCP_IP, TCP_PORT)) 
+
+
+#El servidor siempre debe estar escuchando en caso de una solicitud de un cliente
+while True: 
+    Server.listen(4) 
+    print("Multithreaded Python server : Waiting for connections from clients...") 
+    (conn, (ip,port)) = Server.accept() #coneccion de un cliente
+    recibido = bytes.decode(Server.recv(1024))
+    data = bytes.decode(conn.recv(2048)) 
+    print("Server received data:" + data)
+    MESSAGE = input("Multithreaded Python server : Enter Response from Server/Enter exit:")
+    conn.send(MESSAGE.encode())  # echo """
+
